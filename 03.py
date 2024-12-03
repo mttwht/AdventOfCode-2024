7d3773038daf77549092c153c376f540bfb078e9
Matthew White <Matthew.White02@gmail.com>
2024-12-03 22:37:06 +0000

    Update to solve day 3 part 2

--- 03.py
+++ 03.py
@@ -1,17 +1,20 @@
 import re
 
 with open("input-03.txt", "r") as file:
-    lines = [line.strip() for line in file.readlines()]
+    line = file.read().strip()
 
-# lines = [line.strip() for line in """
-# xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-# """.splitlines()][1:]
-# # Example answer  = 161
+# line = """
+# xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+# """.strip()
+# # Example answer  = 48
 
 
 result = 0
-for line in lines:
-    for x, y in re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', line):
-        result += int(x) * int(y)
+
+line = re.sub(r'don\'t\(\).*?do\(\)', '', line, flags=re.S)
+line = re.sub(r'don\'t\(\).*?$', '', line)
+
+for x, y in re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', line):
+    result += int(x) * int(y)
 
 print(result)

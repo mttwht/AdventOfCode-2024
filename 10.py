fbe1f81ef8a87c68e8e8e2d4bd3b56d986a1fd86
Matthew White <Matthew.White02@gmail.com>
2024-12-10 22:35:12 +0000

    Update solution to solve day 10 part 2

--- 10.py
+++ 10.py
@@ -12,27 +12,29 @@ with open("input-10.txt", "r") as file:
 # 10456732
 # """.splitlines()][1:]
 # # Example answer for part 1 = 36
+# # Example answer for part 2 = 81
 
 
 def find_peaks(map: list[str], pos: tuple[int, int]) -> list[tuple[int, int]]:
     x, y = pos
     if map[y][x] == '9':
-        return set([pos])
+        return [pos]
     
-    peaks = set()
+    peaks = []
     neighbours = [(x, y-1), (x+1, y), (x, y+1), (x-1, y)]
     for xx, yy in [(xxx, yyy) for (xxx, yyy) in neighbours if 0 <= xxx < len(map[y]) and 0 <= yyy < len(map)]:
         if int(map[yy][xx]) == int(map[y][x]) + 1:
-            peaks.update(find_peaks(map, (xx, yy)))
+            peaks.extend(find_peaks(map, (xx, yy)))
     
-    return list(peaks)
+    return peaks
 
 
-total_score = 0
+score1 = score2 = 0
 for y in range(len(lines)):
     for x in [x for x in range(len(lines[y])) if lines[y][x] == '0']:
         peaks = find_peaks(lines, (x, y))
-        score = len(peaks)
-        total_score += score
+        score1 += len(list(set(peaks)))
+        score2 += len(peaks)
 
-print('Part 1:', total_score)
+print('Part 1:', score1)
+print('Part 2:', score2)

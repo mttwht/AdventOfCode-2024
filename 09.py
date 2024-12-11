a4708d0ab4fbd1ae8bc8e89c9b0d39c3051872a9
Matthew White <35046220+mttwht@users.noreply.github.com>
2024-12-11 14:14:53 +0000

    Update to solve day 9 part 2 Still slow but still works!

--- 09.py
+++ 09.py
@@ -5,6 +5,7 @@ with open("input-09.txt", "r") as file:
 # 2333133121414131402
 # """.splitlines()][1:]
 # # Example answer for part 1 = 1928
+# # Example answer for part 2 = 2858
 
 
 def list_file_blocks(disk_map: str) -> list[int]:
@@ -24,11 +25,30 @@ def defrag_file_blocks(file_blocks: list[int]) -> list[int]:
             file_blocks[file_blocks.index(None)] = end_val
     return file_blocks
 
+def defrag_file_blocks_2(file_blocks: list[int]) -> list[int]:
+    start_points = {i: 0 for i in range(1, 10)}
+    for id in reversed(range(1, file_blocks[-1] + 1)):
+        print('Progress', id)
+        index = file_blocks.index(id)
+        length = file_blocks.count(id)
+        for i in range(start_points[length], index):
+            if file_blocks[i:i+length] == [None] * length:
+                file_blocks[i:i+length] = [id] * length
+                file_blocks[index:index+length] = [None] * length
+                start_points[length] = i + length
+                break
+    return file_blocks
+
 def calc_checksum(file_blocks: list[int]) -> int:
-    return sum([i * file_blocks[i] for i in range(len(file_blocks))])
+    return sum([i * file_blocks[i] for i in range(len(file_blocks)) if file_blocks[i] is not None])
 
 
 disk_map = lines[0]
+
 file_blocks = list_file_blocks(disk_map)
 file_blocks = defrag_file_blocks(file_blocks)
-print(calc_checksum(file_blocks))
+print('Part 1:', calc_checksum(file_blocks))
+
+file_blocks = list_file_blocks(disk_map)
+file_blocks = defrag_file_blocks_2(file_blocks)
+print('Part 2:', calc_checksum(file_blocks))

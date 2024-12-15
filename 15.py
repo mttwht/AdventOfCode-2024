df3bf40588436bbcf18ed02abbbe3f5a48934028
Matthew White <Matthew.White02@gmail.com>
2024-12-15 22:42:15 +0000

    Update to solve day 15 part 2

--- 15.py
+++ 15.py
@@ -1,8 +1,9 @@
 with open("input-15.txt", "r") as file:
     lines = [line.strip() for line in file.readlines()]
 
-ROBOT, BOX, WALL, EMPTY = "@O#."
+ROBOT, BOX, WALL, EMPTY, BOX_L, BOX_R = "@O#.[]"
 N,E,S,W = "^>v<"
+X, Y = 0, 1
 
 # lines = [line.strip() for line in """
 # ########
@@ -42,17 +43,29 @@ N,E,S,W = "^>v<"
 # v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
 # """.splitlines()][1:]
 # # Example answer for part 1 = 10092
+# # Example answer for part 2 = 9021
 
 
-def parse(input: list[str]) -> tuple[list[str], tuple[int,int], str]:
+def parse(input: list[str]) -> tuple[list[list[str]], tuple[int,int], str]:
     i_sep = input.index('')
     map = [list(s) for s in input[:i_sep]]
     movements = ''.join(input[i_sep+1:])
-    for y in range(len(lines)):
-        if ROBOT in lines[y]:
-            pos = (lines[y].index(ROBOT), y)
-            break
-    return (map, pos, movements)
+    return (map, get_robot_pos(map), movements)
+
+def get_robot_pos(map: list[list[str]]) -> tuple[int,int]:
+    for y in range(len(map)):
+        if ROBOT in map[y]:
+            return (map[y].index(ROBOT), y)
+
+def widen(map: list[list[str]]) -> list[list[str]]:
+    wider = {
+        WALL: WALL + WALL,
+        BOX: BOX_L + BOX_R,
+        EMPTY: EMPTY + EMPTY,
+        ROBOT: ROBOT + EMPTY,
+    }
+
+    return [list(''.join([wider[s] for s in row])) for row in map]
 
 def next_pos(pos: tuple[int,int], dir: str) -> tuple[int,int]:
     x, y = pos
@@ -66,29 +79,55 @@ def move_robot(map: list[str], pos: tuple[int,int], dir: str) -> tuple[list[str]
     nx, ny = next_pos(pos, dir) # robot move pos
     x, y = nx, ny
 
-    while map[y][x] == BOX:
-        x, y = next_pos((x, y), dir)
-    
     # move to empty square:
     if map[ny][nx] == EMPTY:
         map[ny][nx] = ROBOT
         map[sy][sx] = EMPTY
         pos = (nx, ny)
-    # move and push boxes
-    elif map[y][x] == EMPTY:
-        map[sy][sx] = EMPTY
-        map[ny][nx] = ROBOT
-        map[y][x] = BOX
-        pos = (nx, ny)
-    
+    elif map[ny][nx] == BOX:
+        while map[y][x] == BOX:
+            x, y = next_pos((x, y), dir)
+        # move and push boxes
+        if map[y][x] == EMPTY:
+            map[sy][sx] = EMPTY
+            map[ny][nx] = ROBOT
+            map[y][x] = BOX
+            pos = (nx, ny)
+    elif map[ny][nx] in [BOX_L, BOX_R]:
+        cells_to_move = [pos]
+        for cell in cells_to_move:
+            nx, ny = next_pos(cell, dir)
+            if map[ny][nx] == WALL:
+                cells_to_move.append((nx, ny))
+                break
+            elif map[ny][nx] in [BOX_L, BOX_R]:
+                box_cells = [(nx, ny)]
+                if dir in [N, S]:
+                    if map[ny][nx] == BOX_L: box_cells.append((nx+1, ny))
+                    else: box_cells.append((nx-1, ny))
+                cells_to_move.extend([c for c in box_cells if c not in cells_to_move])
+
+        cell = cells_to_move[-1]
+        if map[cell[Y]][cell[X]] != WALL:
+            for cx, cy in reversed(cells_to_move):
+                nx, ny = next_pos((cx, cy), dir)
+                map[ny][nx] = map[cy][cx]
+                map[cy][cx] = EMPTY
+            pos = (nx, ny)
+
     return map, pos
 
 def sum_box_coords(map: list[str]) -> int:
     coords = []
     for row in range(len(map)):
-        for col in [i for i in range(len(map[row])) if map[row][i] == BOX]:
+        for col in [i for i in range(len(map[row])) if map[row][i] in [BOX, BOX_L]]:
             coords.append(100*row + col)
     return sum(coords)
+
+def print_map(map: list[list[str]], dir: str = None) -> None:
+    print(dir)
+    for row in map:
+        print(''.join(row))
 
 
 map, pos, movements = parse(lines)
@@ -99,3 +138,16 @@ for move in movements:
 answer = sum_box_coords(map)
 
 print('Part 1:', answer)
+
+map, pos, movements = parse(lines)
+map = widen(map)
+pos = get_robot_pos(map)
+
+# print_map(map)
+for move in movements:
+    map, pos = move_robot(map, pos, move)
+    # print_map(map, move)
+
+answer = sum_box_coords(map)
+
+print('Part 2:', answer)

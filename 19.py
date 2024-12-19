b726da70f8a40aa7363b6f6923d8f01b00e76cea
Matthew White <Matthew.White02@gmail.com>
2024-12-19 23:08:38 +0000

    Update to solve day 19 part 2

--- 19.py
+++ 19.py
@@ -16,6 +16,7 @@ input = aoc.read_file_lines("input-19.txt")
 # bbrgwb
 # """)
 # # Example answer for part 1 = 6
+# # Example answer for part 2 = 16
 
 
 def parse(input: list[str]) -> tuple[list[str], list[str]]:
@@ -27,19 +28,9 @@ def parse(input: list[str]) -> tuple[list[str], list[str]]:
             designs.append(line)
     return patterns, designs
 
-# def can_make_design(patterns: list[str], design: str) -> list[list[str]]:
-#     designs = []
-#     for pattern in patterns:
-#         if design == pattern:
-#             designs.append([pattern])
-#         elif design.startswith(pattern):
-#             for d in can_make_design(patterns, design[len(pattern):]):
-#                 designs.append([pattern] + d)
-#     return designs
-
 impossible_patterns = set()
 def can_make_design(patterns: list[str], design: str) -> list[list[str]]:
-    global impossible_patterns
+    global impossible_patterns, possibilities
     if design in impossible_patterns:
         return None
     for pattern in [p for p in patterns if design.startswith(p)]:
@@ -51,14 +42,27 @@ def can_make_design(patterns: list[str], design: str) -> list[list[str]]:
                 return [pattern] + d
     impossible_patterns.add(design)
 
+possibilities = {}
+def how_many_possibilities(patterns: list[str], design: str) -> int:
+    global possibilities
+    if design in impossible_patterns:
+        return 0
+    if design in possibilities:
+        return possibilities[design]
+    count = 0
+    for pattern in [p for p in patterns if design.startswith(p)]:
+        if design == pattern:
+            count += 1
+        elif design.startswith(pattern):
+            count += how_many_possibilities(patterns, design[len(pattern):])
+    possibilities[design] = count
+    return count
+
+
 patterns, designs = parse(input)
-patterns.sort(key=lambda x : len(x), reverse=True)
 
-answer = 0
-for design in designs:
-    solutions = can_make_design(patterns, design)
-    print(design, solutions)
-    if solutions:
-        answer += 1
-
+answer = len([design for design in designs if can_make_design(patterns, design)])
 print('Part 1:', answer)
+
+answer = sum([how_many_possibilities(patterns, design) for design in designs])
+print('Part 2:', answer)
